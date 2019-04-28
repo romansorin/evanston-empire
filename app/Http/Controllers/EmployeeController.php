8e45b5052703e51a030d8e7cb559e336ff92d220
Roman Sorin <rmaximsorin@gmail.com>
2019-04-27 22:02:46 -0400

    Employee model & controller

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Employee;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('employees.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('employees.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store()
+    {
+        $attributes = request()->validate([
+            'name' => ['required', 'string'],
+            'title' => ['required', 'string'],
+            'university' => ['nullable', 'string'],
+            'city_name' => ['nullable', 'string'],
+            'state_name' => ['nullable', 'string'],
+            'description' => ['nullable', 'string']
+        ]);
+
+        Employee::create($attributes);
+
+        return view('employees.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($name)
+    {
+        $employee = Employee::where('name', $name)->first();
+
+        return view('employees.edit', compact('employee'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function update($id) // TODO
+    {
+        $employee = Employee::where('id', $id)->first();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Employee::where('id', $id)->first()->delete();
+        return redirect('/dashboard/employees');
+    }
+}
