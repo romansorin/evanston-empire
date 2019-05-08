6df85c8e48604c36d6c84b018097f5b8ba00b1e3
Roman Sorin <988244@students.mentorschools.org>
2019-05-07 20:35:54 -0400

    formatting, i guess?

--- resources/views/pages/team.blade.php
+++ resources/views/pages/team.blade.php
@@ -1,74 +1,59 @@
-@extends('app.main') 
-@section('title', 'Meet The Team') 
+@extends('app.main')
+@section('title', 'Meet The Team')
 @section('stylesheets')
 <link rel="stylesheet" href="{{ asset('css/pages/team.css') }}">
 @endsection
- 
+
 @section('content')
 
 <div class="container">
-	<div class="jumbotron hero">
-		<div class="row">
-			<div class="col-12 text-center">
-				<h1 class="title title--light weight-bold">Meet The Team</h1>
-			</div>
-		</div>
-	</div>
+    <div class="jumbotron hero">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1 class="title title--light weight-bold">Meet The Team</h1>
+            </div>
+        </div>
+    </div>
 </div>
 </div>
 <div class="container">
-	<section class="main-content">
-		<div class="row">
-			<div class="col-12 text-center">
-				<blockquote class="blockquote ml-auto mr-auto">
-					<p class="display-4 blockquote-text weight-light--italic">“We make it our mission to have an impact on the world.”</p>
-				</blockquote>
-			</div>
-		</div>
-	</section>
+    <section class="main-content">
+        <div class="row">
+            <div class="col-12 text-center">
+                <blockquote class="blockquote ml-auto mr-auto">
+                    <p class="display-4 blockquote-text weight-light--italic">“We make it our mission to have an impact
+                        on the world.”</p>
+                </blockquote>
+            </div>
+        </div>
+    </section>
 </div>
 <div class="container-fluid">
-	<section class="main-content">
-		<div class="container">
-			<div class="row section-heading pl-2" id="employees-heading">
-				<h3 class="title weight-semibold">Our Employees</h3>
-			</div>
-		</div>
-		<div class="row">
-			<section class="center slider">
-				<div>
-					<img class="img-fluid employee-photo" src="https://images.pexels.com/photos/2222745/pexels-photo-2222745.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
-				</div>
-				<div>
-					<img class="img-fluid employee-photo " src="https://images.pexels.com/photos/2222745/pexels-photo-2222745.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
-				</div>
-				<div>
-					<img class="img-fluid employee-photo" src="https://images.pexels.com/photos/2222745/pexels-photo-2222745.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
-				</div>
-				<div>
-					<img class="img-fluid employee-photo" src="https://images.pexels.com/photos/2222745/pexels-photo-2222745.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
-				</div>
-				<div>
-					<img class="img-fluid employee-photo" src="https://images.pexels.com/photos/2222745/pexels-photo-2222745.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
-				</div>
-				<div>
-					<img class="img-fluid employee-photo" src="https://images.pexels.com/photos/2222745/pexels-photo-2222745.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
-				</div>
-				<div>
-					<img class="img-fluid employee-photo" src="https://images.pexels.com/photos/2222745/pexels-photo-2222745.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
-				</div>
-				<div>
-					<img class="img-fluid employee-photo" src="https://images.pexels.com/photos/2222745/pexels-photo-2222745.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
-				</div>
-				<div>
-					<img class="img-fluid employee-photo" src="https://images.pexels.com/photos/2222745/pexels-photo-2222745.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
-				</div>
-			</section>
-		</div>
-	</section>
+    <section class="main-content">
+        <div class="container">
+            <div class="row section-heading pl-2" id="employees-heading">
+                <h3 class="title weight-semibold">Our Employees</h3>
+            </div>
+        </div>
+        <div class="row">
+            <section class="center slider">
+                @php
+                use App\Employee;
+                $employees = Employee::all();
+                @endphp
+                @foreach ($employees as $employee)
+                <div>
+                    <span>{{ $employee->name }}</span>
+                    <img class="img-fluid employee-photo"
+                        src="https://images.pexels.com/photos/2222745/pexels-photo-2222745.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
+                </div>
+                @endforeach
+            </section>
+        </div>
+    </section>
 </div>
 @endsection
- 
+
 @section('scripts')
 <script src="{{ asset('js/slick.js') }}"></script>
 @endsection
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,119 +1,128 @@
-@extends('app.main') 
+@extends('app.main')
 @section('stylesheets')
 <link rel="stylesheet" href="{{ asset('css/pages/home.css') }}">
 @endsection
- 
-@section('title', 'Home') 
+
+@section('title', 'Home')
 @section('content')
 <div class="container">
-	<div class="jumbotron hero pl-0">
-		<div class="row">
-			<div class="col-md-6 col-sm-10">
-				<h1 class="title title--light weight-semibold">Your <span class="weight-bold">next job</span><br/>is here.</h1>
-				<h2 class="subtitle subtitle--light">We are a team of passionate students with the goal of creating rich and real opportunities.</h2>
-				<button class="btn cta-btn-light weight-semibold">Get in touch</button>
-			</div>
-			<div class="col-6">
+    <div class="jumbotron hero pl-0">
+        <div class="row">
+            <div class="col-md-6 col-sm-10">
+                <h1 class="title title--light weight-semibold">Your <span class="weight-bold">next job</span><br />is
+                    here.</h1>
+                <h2 class="subtitle subtitle--light">We are a team of passionate students with the goal of creating rich
+                    and real opportunities.</h2>
+                <button class="btn cta-btn-light weight-semibold">Get in touch</button>
+            </div>
+            <div class="col-6">
 
-			</div>
-		</div>
-	</div>
+            </div>
+        </div>
+    </div>
 </div>
 </div>
 <div class="container">
-	<section class="main-content">
-		<div class="row section-heading">
-			<div class="col-12 text-center">
-				<h3 class="title weight-semibold">Explore Our Opportunities</h3>
-				<div class="border-bottom-purple ml-auto mr-auto"></div>
-			</div>
-		</div>
-		<div class="row section-content" id="explore-opportunities-card-wrapper">
-			<div class="col-xl-3 col-md-6 col-sm-12">
-				<div class="card ml-xl-0 mr-xl-0 mx-auto">
-					<div class="card-img">
-						<img class="img-fluid" src="https://images.pexels.com/photos/2097116/pexels-photo-2097116.jpeg?cs=srgb&dl=architectural-design-architecture-building-2097116.jpg&fm=jpg"
-						 alt="filler image">
-					</div>
-					<div class="card-body text-center">
-						<div class="card-info mx-auto">
-							<h5 class="card-title">Special title treatment</h5>
-							<a href="#" class="weight-semibold">Go somewhere</a>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-xl-3 col-md-6 col-sm-12">
-				<div class="card ml-xl-0 mr-xl-0 mx-auto">
-					<div class="card-img">
-						<img class="img-fluid" src="https://images.pexels.com/photos/2097116/pexels-photo-2097116.jpeg?cs=srgb&dl=architectural-design-architecture-building-2097116.jpg&fm=jpg"
-						 alt="filler image">
-					</div>
-					<div class="card-body text-center">
-						<div class="card-info mx-auto">
-							<h5 class="card-title">Special title treatment</h5>
-							<a href="#" class="weight-semibold">Go somewhere</a>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-xl-3 col-md-6 col-sm-12">
-				<div class="card ml-xl-0 mr-xl-0 mx-auto">
-					<div class="card-img">
-						<img class="img-fluid" src="https://images.pexels.com/photos/2097116/pexels-photo-2097116.jpeg?cs=srgb&dl=architectural-design-architecture-building-2097116.jpg&fm=jpg"
-						 alt="filler image">
-					</div>
-					<div class="card-body text-center">
-						<div class="card-info mx-auto">
-							<h5 class="card-title">Special title treatment</h5>
-							<a href="#" class="weight-semibold">Go somewhere</a>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-xl-3 col-md-6 col-sm-12">
-				<div class="card ml-xl-0 mr-xl-0 mx-auto">
-					<div class="card-img">
-						<img class="img-fluid" src="https://images.pexels.com/photos/2097116/pexels-photo-2097116.jpeg?cs=srgb&dl=architectural-design-architecture-building-2097116.jpg&fm=jpg"
-						 alt="filler image">
-					</div>
-					<div class="card-body text-center">
-						<div class="card-info mx-auto">
-							<h5 class="card-title">Special title treatment</h5>
-							<a href="#" class="weight-semibold">Go somewhere</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-	<section class="main-content">
-		<div class="row section-heading">
-			<div class="col-12 text-center">
-				<h3 class="title weight-semibold">What We Do</h3>
-				<div class="border-bottom-purple ml-auto mr-auto"></div>
-			</div>
-		</div>
-		<div class="row section-content">
-			<div class="col-12 text-center">
-				<blockquote class="blockquote ml-auto mr-auto">
-					<p class="blockquote-text weight-light--italic">Profit is the ignition system of your economic engine.</p>
-				</blockquote>
-			</div>
-			<div class="col-4" id="what-we-do-info">
-				<h3 class="title">Make your success as simple as you can clap</h3>
-				<p class="subtitle">Writing extra info here to simulate these types of text blocks to illustrate the idea of more content to sell someone
-					on a product. And more, for a fourth line</p>
-				<a href class="link weight-bold">View more benefits <span class="oi oi-arrow-right" title="arrow right" aria-hidden="true" id="view-more-arrow"></span></a>
-			</div>
-			<div class="col-8">
-			</div>
-		</div>
-		<div class="row text-center">
-			<div class="col-12">
-				<button class="btn cta-btn-primary">Learn more</button>
-			</div>
-		</div>
-	</section>
+    <section class="main-content">
+        <div class="row section-heading">
+            <div class="col-12 text-center">
+                <h3 class="title weight-semibold">Explore Our Opportunities</h3>
+                <div class="border-bottom-purple ml-auto mr-auto"></div>
+            </div>
+        </div>
+        <div class="row section-content" id="explore-opportunities-card-wrapper">
+            <div class="col-xl-3 col-md-6 col-sm-12">
+                <div class="card ml-xl-0 mr-xl-0 mx-auto">
+                    <div class="card-img">
+                        <img class="img-fluid"
+                            src="https://images.pexels.com/photos/2097116/pexels-photo-2097116.jpeg?cs=srgb&dl=architectural-design-architecture-building-2097116.jpg&fm=jpg"
+                            alt="filler image">
+                    </div>
+                    <div class="card-body text-center">
+                        <div class="card-info mx-auto">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <a href="#" class="weight-semibold">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-6 col-sm-12">
+                <div class="card ml-xl-0 mr-xl-0 mx-auto">
+                    <div class="card-img">
+                        <img class="img-fluid"
+                            src="https://images.pexels.com/photos/2097116/pexels-photo-2097116.jpeg?cs=srgb&dl=architectural-design-architecture-building-2097116.jpg&fm=jpg"
+                            alt="filler image">
+                    </div>
+                    <div class="card-body text-center">
+                        <div class="card-info mx-auto">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <a href="#" class="weight-semibold">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-6 col-sm-12">
+                <div class="card ml-xl-0 mr-xl-0 mx-auto">
+                    <div class="card-img">
+                        <img class="img-fluid"
+                            src="https://images.pexels.com/photos/2097116/pexels-photo-2097116.jpeg?cs=srgb&dl=architectural-design-architecture-building-2097116.jpg&fm=jpg"
+                            alt="filler image">
+                    </div>
+                    <div class="card-body text-center">
+                        <div class="card-info mx-auto">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <a href="#" class="weight-semibold">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-6 col-sm-12">
+                <div class="card ml-xl-0 mr-xl-0 mx-auto">
+                    <div class="card-img">
+                        <img class="img-fluid"
+                            src="https://images.pexels.com/photos/2097116/pexels-photo-2097116.jpeg?cs=srgb&dl=architectural-design-architecture-building-2097116.jpg&fm=jpg"
+                            alt="filler image">
+                    </div>
+                    <div class="card-body text-center">
+                        <div class="card-info mx-auto">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <a href="#" class="weight-semibold">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="main-content">
+        <div class="row section-heading">
+            <div class="col-12 text-center">
+                <h3 class="title weight-semibold">What We Do</h3>
+                <div class="border-bottom-purple ml-auto mr-auto"></div>
+            </div>
+        </div>
+        <div class="row section-content">
+            <div class="col-12 text-center">
+                <blockquote class="blockquote ml-auto mr-auto">
+                    <p class="blockquote-text weight-light--italic">Profit is the ignition system of your economic
+                        engine.</p>
+                </blockquote>
+            </div>
+            <div class="col-4" id="what-we-do-info">
+                <h3 class="title">Make your success as simple as you can clap</h3>
+                <p class="subtitle">Writing extra info here to simulate these types of text blocks to illustrate the
+                    idea of more content to sell someone
+                    on a product. And more, for a fourth line</p>
+                <a href class="link weight-bold">View more benefits <span class="oi oi-arrow-right" title="arrow right"
+                        aria-hidden="true" id="view-more-arrow"></span></a>
+            </div>
+            <div class="col-8">
+            </div>
+        </div>
+        <div class="row text-center">
+            <div class="col-12">
+                <button class="btn cta-btn-primary">Learn more</button>
+            </div>
+        </div>
+    </section>
 </div>
 @endsection
