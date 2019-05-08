8f15f9c92fd33b61eec1d5221c671e0e77001618
Roman Sorin <988244@students.mentorschools.org>
2019-05-08 10:09:53 -0400

    first footer iteration

---
+++ resources/views/layouts/main/footer.blade.php
@@ -0,0 +1,53 @@
+<footer class="page-footer footer-wrapper pt-4">
+    <div class="container text-center text-md-left">
+        <div class="row text-center text-md-left mt-3 pb-3">
+            <div class="col-md-6 col-lg-4 col-xl-4 mx-auto mt-3">
+                <h1 class="mb-4 pt-0 mt-0 weight-semibold">Evanston Empire</h1>
+                <div class="mt-3 mb-0">
+                    <p>123 Main St., New York, NY 10012</p>
+                    <p>info@evanstonempire.com</p>
+                    <p>123-456-7890</p>
+                </div>
+            </div>
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 class="mb-4 footer-heading weight-semibold">Company</h6>
+                <p>
+                    <a href="/what-we-do">What We Do</a>
+                </p>
+                <p>
+                    <a href="/meet-the-team">Meet The Team</a>
+                </p>
+                <p>
+                    <a href="/gallery">Gallery</a>
+                </p>
+                <p>
+                    <a href="/apply">Apply</a>
+                </p>
+                <p>
+                    <a href="/faq">FAQ</a>
+                </p>
+            </div>
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 class="mb-4 footer-heading weight-semibold">Legal</h6>
+                <p>
+                    <a href="/terms-conditions">Terms & Conditions</a>
+                </p>
+                <p>
+                    <a href="/privacy-policy">Privacy Policy</a>
+                </p>
+            </div>
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 class="mb-4 footer-heading weight-semibold">Social</h6>
+                <p>
+                    <a href="https://www.instagram.com">Instagram</a>
+                </p>
+                <p>
+                    <a href="https://www.twitter.com">Twitter</a>
+                </p>
+                <p>
+                    <a href="https://www.linkedin.com">LinkedIn</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
