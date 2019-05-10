1bb9ebb6dd516041defe95e4704662910b67e5b9
Roman Sorin <rmaximsorin@gmail.com>
2019-05-10 19:15:55 -0400

    init header commit

--- resources/views/layouts/main/header.blade.php
+++ resources/views/layouts/main/header.blade.php
@@ -29,3 +29,33 @@
 		</div>
 	</nav>
 </div>
+
+<!-- The overlay -->
+<div id="myNav" class="overlay">
+
+		<!-- Button to close the overlay navigation -->
+		<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+	  
+		<!-- Overlay content -->
+		<div class="overlay-content">
+		  <a href="#">About</a>
+		  <a href="#">Services</a>
+		  <a href="#">Clients</a>
+		  <a href="#">Contact</a>
+		</div>
+	  
+	  </div>
+	  
+	  <!-- Use any element to open/show the overlay navigation menu -->
+	  <span onclick="openNav()">open</span>
+	  
+<script>
+/* Open */
+function openNav() {
+  document.getElementById("myNav").style.display = "block";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("myNav").style.display = "none";
+}</script>
