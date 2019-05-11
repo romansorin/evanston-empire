26861705818a51c57cacbe2239df2f18737783c0
Roman Sorin <rmaximsorin@gmail.com>
2019-05-10 21:27:38 -0400

    i cri

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -32,6 +32,12 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+
 if (token) {
     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
