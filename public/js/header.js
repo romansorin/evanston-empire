190ea0fda0e47ea55320a1bb8da9a6d79116f670
Roman Sorin <rmaximsorin@gmail.com>
2019-05-11 17:08:50 -0400

    minor header work

--- public/js/header.js
+++ public/js/header.js
@@ -96,6 +96,19 @@
 document.querySelector('.navbar-toggler').addEventListener('click', function () {
   document.querySelector('.navbar-toggler').classList.toggle('is-active');
 });
+$(document).ready(function () {
+  $('#menu-toggle').click(function () {
+    if ($(this).hasClass('open')) {
+      $(this).removeClass('open');
+      $('nav.mobile').removeClass('mobile--open');
+      $('.header-wrapper').css('background-color', 'transparent');
+    } else {
+      $(this).addClass('open');
+      $('nav.mobile').addClass('mobile--open');
+      $('.header-wrapper').css('background-color', 'white');
+    }
+  });
+});
 
 /***/ }),
 
--- resources/views/layouts/main/header.blade.php
+++ resources/views/layouts/main/header.blade.php
@@ -1,17 +1,41 @@
 <div class="header-wrapper container">
-	<nav class="navbar navbar-expand-lg pl-0 pr-0">
+	<nav class="navbar navbar-expand-lg pl-0 pr-0 ">
 		<a class="navbar-brand" href="/">LOGO</a>
+		<ul style="list-style: none">
+			<li class="mobile-nav">
+				<button class="ml-auto navbar-toggler hamburger hamburger--stand" type="button" id="menu-toggle"
+					data-toggle="collapse" data-target="#menu" aria-controls="menu" aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span class="hamburger-box">
+						<span class="hamburger-inner"></span>
+					</span>
+				</button></li>
+		</ul>
 
-		<button class="ml-auto navbar-toggler hamburger hamburger--stand" type="button" id="menu-toggle"
-			data-toggle="collapse" data-target="#menu" aria-controls="menu" aria-expanded="false"
-			aria-label="Toggle navigation">
-			<span class="hamburger-box">
-				<span class="hamburger-inner"></span>
-			</span>
-		</button>
+		<ul class="navbar-nav ml-auto d-none d-lg-flex">
+			<li class="nav-item">
+				<a class="nav-link" href="/what-we-do">What We Do</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/meet-the-team">Meet The Team</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/gallery">Gallery</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/apply">Apply</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/faq">FAQ</a>
+			</li>
+		</ul>
+	</nav>
 
-		<div class="collapse navbar-collapse" id="menu">
-			<ul class="navbar-nav ml-auto">
+
+
+	<div class="collapse navbar-collapse" id="menu">
+		<nav class="mobile">
+			<ul>
 				<li class="nav-item">
 					<a class="nav-link" href="/what-we-do">What We Do</a>
 				</li>
@@ -28,6 +52,7 @@
 					<a class="nav-link" href="/faq">FAQ</a>
 				</li>
 			</ul>
-		</div>
-	</nav>
+			</ul>
+		</nav>
+	</div>
 </div>
